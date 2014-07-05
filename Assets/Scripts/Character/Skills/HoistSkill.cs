50938688a7a8b5535aeee5724c9362e525f4c5ac
Tibor Hencz <tibor@applifier.com>
2014-07-05 11:29:15 +0300

    Hoists updated

--- Assets/Scripts/Character/Skills/HoistSkill.cs
+++ Assets/Scripts/Character/Skills/HoistSkill.cs
@@ -64,7 +64,7 @@ public class HoistSkill : Skill {
 				if (!animator.GetCurrentAnimatorStateInfo(0).IsName(stateName)) {
 					hoisting = false;
 					hoistStarted = false;
-					transform.position = hoistPos + new Vector3(0f, high ? 0.2f : 0.2f, 0f);
+					transform.position = hoistPos + new Vector3(0f, high ? 0.2f : 0.1f, 0f);
 					Player.SpriteTransform.localPosition = Vector2.zero;
 				}
 			}
